b74a926b3f5b886ed3ce3e4c6a9facace8a8ff84
mohamedommran <mohamed.ommran@gmail.com>
2022-01-27 09:26:15 +0100

    First commit of the project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+namespace QuizzJSON
+{
+    public class Json
+    {
+        public Dictionary<string,User> users { get; set; }
+        public string[] questions { get; set; }
+        public string[] answers { get; set; }
+    }
+
+    public class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileName = "C:/Users/Mody/Desktop/QuizzJSON/quizz.json";
+            string jsonString = File.ReadAllText(fileName);
+            Json json = JsonSerializer.Deserialize<Json>(jsonString);
+
+
+            Console.WriteLine(json.users["admin"].name);
+        }
+    }
+}
